3480980873ebbeecc1818c8efb5a09383c84b9cd
Kevin Bell <bellKevin@pm.me>
2022-06-15 18:38:41 +0000

    Update PennyJarDriver.java

--- PennyJarDriver/src/pennyjardriver/PennyJarDriver.java
+++ PennyJarDriver/src/pennyjardriver/PennyJarDriver.java
@@ -6,8 +6,9 @@ public class PennyJarDriver {
         PennyJar pennyJar1 = new PennyJar();
         PennyJar pennyJar2 = new PennyJar();
         PennyJar.setGoal(4000);
-        pennyJar1.addPenny();
-        pennyJar1.addPenny();
+        for (int i = 0; i < 4010; i++) {
+            pennyJar1.addPenny();
+        }
         pennyJar2.addPenny();
         System.out.println(pennyJar1.getPennies());
         System.out.println(PennyJar.getAllPennies());
